7b5a357285cda83825b03faa22cb5f4e80f7b0d3
rejhe <rejhe@channing.harvard.edu>
2024-08-12 10:58:41 -0400

    add other sets

--- R/S_sets.R
+++ R/S_sets.R
@@ -33,3 +33,70 @@
 "S_80_100"
 
 
+#' Set of slide-effect-susceptible CpG sites "S_60_80" as derived by Hecker et al. 2023
+#'
+#' This set of slide-effect-susceptible CpG sites was 
+#' derived by Hecker et al. 2023. S_60_80 contains all CpG sites where at least one out of 9
+#' datasets estimated a proportion of >60% variance explained by slide effects, excluding S_80_100.
+#'
+#' @docType data
+#'
+#' @usage data(S_60_80)
+#'
+#'
+#' @references Hecker et al. (2023) 
+#'
+#'
+"S_60_80"
+
+
+#' Set of slide-effect-susceptible CpG sites "S_40_60" as derived by Hecker et al. 2023
+#'
+#' This set of slide-effect-susceptible CpG sites was 
+#' derived by Hecker et al. 2023. S_40_60 contains all CpG sites where at least one out of 9
+#' datasets estimated a proportion of >40% variance explained by slide effects, excluding S_80_100 and S_60_80.
+#'
+#' @docType data
+#'
+#' @usage data(S_40_60)
+#'
+#'
+#' @references Hecker et al. (2023) 
+#'
+#'
+"S_40_60"
+
+
+#' Set of slide-effect-susceptible CpG sites "S_20_40" as derived by Hecker et al. 2023
+#'
+#' This set of slide-effect-susceptible CpG sites was 
+#' derived by Hecker et al. 2023. S_20_40 contains all CpG sites where at least one out of 9
+#' datasets estimated a proportion of >20% variance explained by slide effects, excluding S_80_100, S_60_80, and S_40_60.
+#'
+#' @docType data
+#'
+#' @usage data(S_20_40)
+#'
+#'
+#' @references Hecker et al. (2023) 
+#'
+#'
+"S_20_40"
+
+
+#' Set of slide-effect-susceptible CpG sites "S_0_20" as derived by Hecker et al. 2023
+#'
+#' This set of slide-effect-susceptible CpG sites was 
+#' derived by Hecker et al. 2023. S_0_20 contains all CpG sites excluding S_80_100, S_60_80, S_40_60, and S_20_40.
+#'
+#' @docType data
+#'
+#' @usage data(S_0_20)
+#'
+#'
+#' @references Hecker et al. (2023) 
+#'
+#'
+"S_0_20"
+
+
